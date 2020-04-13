e72d27b3d9fc460d870ecd9793059aa65d5f17dc
mehedi rana <mdmehedirana@gmail.com>
2020-04-14 05:57:17 +0600

    add class

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    );
+  }
 }
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
