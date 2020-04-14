cf52fd456d4e58bc42dc2b31ebbbe2519b47c888
mehedi rana <mdmehedirana@gmail.com>
2020-04-14 06:02:26 +0600

    add native paper and appbar

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Appbar } from 'react-native-paper';
+import { AppbarContent } from 'react-native-paper/lib/typescript/src/components/Appbar/AppbarContent';
 
 class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        <Appbar.Header>
+          <AppbarContent
+          title="TODO List"
+          />
+        </Appbar.Header>
         <Text>Open up App.js to start working on your app!</Text>
       </View>
     );
