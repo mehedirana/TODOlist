325157d218406a147cb8105f3a81d31b0a6b372c
mehedi rana <mdmehedirana@gmail.com>
2020-04-14 06:40:36 +0600

    add input, button and icon

--- App.js
+++ App.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { Appbar } from 'react-native-paper';
+import { Appbar, TextInput, Button } from 'react-native-paper';
+import '../../a/todoList/assets/save.png';
 
 class App extends React.Component {
+
+  state = {
+    text: 'loading'
+  }
   render() {
     return (
       <View style={styles.container}>
         <Appbar.Header>
-        <Appbar.Content
-          title="ToDo List"
-          subtitle="Subtitle"
+          <Appbar.Content
+            title="ToDo List"
+            subtitle="Subtitle"
+          />
+        </Appbar.Header>
+        <TextInput
+          label=''
+          onChangeText={text => this.setState({ text })}
         />
-      </Appbar.Header>
+        <Button icon={require('../todoList/assets/save.png')} mode="contained" onPress={() => console.log('Pressed')}>
+          Save me
+  </Button>
+
+        <Text>{this.state.text}</Text>
       </View>
     );
   }
@@ -27,3 +41,4 @@ const styles = StyleSheet.create({
     // justifyContent: 'center',
   },
 });
+/**mode="contained" onPress={() => console.log('Pressed')} */
